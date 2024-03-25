9613ac150c1ca3fed44e82780d4a3ab195d60674
Zarika14 <91460861+Zarika14@users.noreply.github.com>
2024-03-25 16:55:23 +0530

    changes made

--- DAY 1/Excersise1.py
+++ DAY 1/Excersise1.py
@@ -1,5 +1,4 @@
 class Laptop:
-    dicount_percent = 10
     def __init__(self,brand_name, model_name,price):
         self.brand_name = brand_name
         self.model_name = model_name
@@ -7,17 +6,15 @@ class Laptop:
         self.laptop_name = brand_name +' '+ model_name
         self.toatal_description = brand_name +' '+ model_name + ' ' + str(price)
     
-    def discount(self):
-        # new_price = self.price - (self.price * Laptop.dicount_percent/100 )
-        new_price = self.price - (self.price * self.dicount_percent/100 ) 
+    def discount(self,num):
+        new_price = self.price - (self.price * num/100 ) 
         return new_price
         
 l1 = Laptop("HP","pavilion gaming", 70000)        
 
 l2 = Laptop("asus","asus tuf",54000)
 
-print(l2.discount())
-print(l1.__dict__)
+print(l2.discount(10))
 
 # print(f"Price of Hp laptop is {l1.price} and price of asus laptop is {l2.price}")
 # print(f"Name of my laptop is {l1.laptop_name}")
--- DAY 1/classVariable.py
+++ DAY 1/classVariable.py
@@ -1,18 +1,41 @@
-class Circle:
-    pi = 3.14
-    def __init__(self,radius):
-        self.radius = radius
+# class Circle:
+#     pi = 3.14
+#     def __init__(self,radius):
+#         self.radius = radius
         
         
         
-    def area(self):
-        return  Circle.pi *self.radius*self.radius    
+#     def area(self):
+#         return  Circle.pi *self.radius*self.radius    
     
-    def circumference(self):
-        return 2*Circle.pi*self.radius
+#     def circumference(self):
+#         return 2*Circle.pi*self.radius
 
-c1 = Circle(4)
+# c1 = Circle(4)
 
-print(c1.area())
+# print(c1.area())
+
+
+
+class Laptop:
+    dicount_percent = 10
+    def __init__(self,brand_name, model_name,price):
+        self.brand_name = brand_name
+        self.model_name = model_name
+        self.price = price
+        self.laptop_name = brand_name +' '+ model_name
+        self.toatal_description = brand_name +' '+ model_name + ' ' + str(price)
+    
+    def discount(self):
+        # new_price = self.price - (self.price * Laptop.dicount_percent/100 )
+        new_price = self.price - (self.price * self.dicount_percent/100 ) 
+        return new_price
+        
+l1 = Laptop("HP","pavilion gaming", 70000)        
+
+l2 = Laptop("asus","asus tuf",54000)
+
+print(l2.discount())
+print(l1.__dict__)
 
     
