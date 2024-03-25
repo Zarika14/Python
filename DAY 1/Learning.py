6102a2d5223ea6ed3ec850c15c2e8aaacfd2928f
Zarika14 <91460861+Zarika14@users.noreply.github.com>
2024-03-25 14:25:13 +0530

    concatenation

--- DAY 1/Learning.py
+++ DAY 1/Learning.py
@@ -94,10 +94,14 @@ class Laptop:
         self.brand_name = brand_name
         self.model_name = model_name
         self.price = price
+        self.laptop_name = brand_name +' '+ model_name
+        self.toatal_description = brand_name +' '+ model_name + ' ' + str(price)
         
 l1 = Laptop("HP","pavilion gaming", 70000)        
 
 l2 = Laptop("asus","asus tuf",54000)
 
-print(f"price of Hp laptop is {l1.price} and price of asus laptop is {l2.price}")
+print(f"Price of Hp laptop is {l1.price} and price of asus laptop is {l2.price}")
+print(f"Name of my laptop is {l1.laptop_name}")
+print(l2.toatal_description)
     
